986b5be7415fcdd320499d7b05b3fd40edced00d
root <root@localhost.localdomain>
2018-05-13 04:14:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rohan_maske423/rohimageslider/HomeActivity.java
@@ -0,0 +1,8 @@
+package com.example.rohan_maske423.rohimageslider;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity{
+
+}
---
+++ app/src/main/java/com/example/rohan_maske423/rohimageslider/PreferenceManager.java
@@ -0,0 +1,38 @@
+package com.example.rohan_maske423.rohimageslider;
+
+/**
+ * Created by root on 19/3/18.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceManager {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "intro_slider-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    public PreferenceManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
